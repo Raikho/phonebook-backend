440cc6c82fcf3f80a102a34913f912acf5c151f2
Mateen <zahidx297@yahoo.com>
2023-11-01 06:44:22 -0400

    implement phonebook-backend step 1 & 2

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+
+const phonebook = [
+	{ 
+		"id": 1,
+		"name": "Arto Hellas", 
+		"number": "040-123456"
+	},
+	{ 
+		"id": 2,
+		"name": "Ada Lovelace", 
+		"number": "39-44-5323523"
+	},
+	{ 
+		"id": 3,
+		"name": "Dan Abramov", 
+		"number": "12-43-234345"
+	},
+	{ 
+		"id": 4,
+		"name": "Mary Poppendieck", 
+		"number": "39-23-6423122"
+	}
+]
+
+app.get('/api/persons', (req, res) => {
+	console.log('getting persons from phonebook...')
+  res.send(phonebook)
+})
+
+app.get('/info', (req, res) => {
+	console.log('getting info...')
+	const numPeople = phonebook.length
+	const date = new Date().toString()
+
+	res.send(`<div>Phonebook has info for ${numPeople} people</div><br /><div>${date}</div>`)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+   console.log('Server running on port 3001...')
+})
