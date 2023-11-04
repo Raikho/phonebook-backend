7799c102c6fc9b1744534888f76c1a2980b7f949
Mateen <zahidx297@yahoo.com>
2023-11-04 05:48:50 -0400

    implement phonebook database step 8

--- backend/models/person.js
+++ backend/models/person.js
@@ -16,8 +16,11 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        // TODO: format
-        minLength: 1,
+        validate: {
+            validator: v => /^\d{2,3}-\d*$/.test(v), 
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        minLength: 8,
         required: true,
     },
 })
